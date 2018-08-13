0a5fd5a8643ce88f8e0d5dbe093a37ef0c7b6158
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-08-13 10:55:52 -0400

    Cleanup TranslationEditorTest

--- tests/Integration/TranslationEditorTest.php
+++ tests/Integration/TranslationEditorTest.php
@@ -7,6 +7,7 @@ class TranslationEditorTest extends TestCase
     public function testItIsDisabledByDefault()
     {
         $enabled = $this->app['config']->get('translation-editor.enabled');
+
         $this->assertFalse($enabled);
     }
 }
