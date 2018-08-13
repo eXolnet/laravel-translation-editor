544dd8ba3a1841a03b610ee3d63e63d3c2530bb1
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-08-13 10:35:51 -0400

    Document the translation-editor.enable configuration

--- config/translation-editor.php
+++ config/translation-editor.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Translation Editor Enable
+    |--------------------------------------------------------------------------
+    |
+    | This value determine if the Translation Editor will be enabled or not. If
+    | disabled, translations variables will be displayed using Laravel's
+    | translation module without the possibility to edit them.
+    |
+    */
+
     'enabled' => env('TRANSLATION_EDITOR_ENABLED', false),
+
 ];
