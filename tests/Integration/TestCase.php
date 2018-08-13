04b8c9b18331ea137403d535fde8e8e0820fef9d
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2018-08-13 10:39:40 -0400

    Make Integration\TestCase abstract

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -5,7 +5,7 @@ namespace Exolnet\Translation\Editor\Tests\Integration;
 use Exolnet\Translation\Editor\TranslationEditorServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
     /**
      * @param \Illuminate\Foundation\Application $app
